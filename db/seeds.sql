a1f50d0fecca26893f919c43e9bd40e023409f95
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-02-04 18:30:24 +0000

    added the burger data for table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Doughnut Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tex-Mex Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', FALSE);
