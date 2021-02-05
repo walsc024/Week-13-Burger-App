592df3ab0d150cd294cb3417bcb06c8801fc0a61
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-02-05 18:22:32 +0000

    set up express server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require("express");
+var routes = require("./controllers/burger_controllers")
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
+app.use(express.static("Public"))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
+var exphbs = require("express-handlebars")
+app.engine("handlebars", exphbs({defaultLayout: "main"}))
+app.set("view engine", "handlebars") 
 
-
-
+app.use(routes)
 
 
 
