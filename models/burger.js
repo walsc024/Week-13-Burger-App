410063061188cff471285f2e1acd6fd25b14fa2c
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-02-07 20:22:50 +0000

    changes to orm

--- models/burger.js
+++ models/burger.js
@@ -6,20 +6,19 @@ var burger = {
             cb(res);
         });
     },
-
-
-
-create: function (name, cb) {
-    orm.create("burgers", [
-        "burger_name", "devoured"
-    ], [
-        name, false
-    ], cb);
-},
-update: function (id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-        devoured: true
-    }, condition, cb);
-}
+    create: function (name, cb) {
+        orm.create("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, condition, cb);
+    }
 };
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 var express = require("express");
-var routes = require("./controllers/burger_controllers")
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static("Public"))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-var exphbs = require("express-handlebars")
-app.engine("handlebars", exphbs({defaultLayout: "main"}))
-app.set("view engine", "handlebars") 
-
-app.use(routes)
-
+// Parse application body
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("Listening on port: ", PORT);
